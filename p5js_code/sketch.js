203b679f03e151184bd5a9a49fe1dfb9943f1854
AlJf-the-Coder <agjafri@up.edu.ph>
2025-05-02 21:49:40 +0800

    implement depth buffering in p5js code and update indices of cube.json

--- p5js_code/sketch.js
+++ p5js_code/sketch.js
@@ -6,11 +6,13 @@ function preload() {
 }
 
 function setup() {
-  const width = 600;
-  const height = 600;
+  const width = 400;
+  const height = 400;
+  frame_num = 0;
   createCanvas(width, height);
   camera = {base: [[0],[0],[0]], angles: [-Math.PI/4,0,0], viewVolume: {N:0, F:width, L:-width/2, R:width/2, T:height/2, B:-height/2}}
   obj = objects.obj1;
+  depthBuffer = Array(height).fill().map(()=>Array(width).fill(Infinity));
   //make each vertex homogeneous
   camera.base.push([1]);
   obj.vertices.forEach((vertex)=>vertex.push([1]));
@@ -19,12 +21,17 @@ function setup() {
 function draw() {
   //draw a sky blue background
   background(135, 206, 235);
+  textAlign(LEFT, TOP);
+  text("frame_num: " + frame_num, 0, 0);
+  frame_num++;
 
-  let camRotate = [0.01, 0.01, 0.01];
+  depthBuffer = Array(height).fill().map(()=>Array(width).fill(Infinity));
+
+  let camRotate = [0.1, -0.1, 0.1];
   moveCamera(camera, undefined, camRotate);
-  let objRotate = [0.01, 0.01, 0.01];
+  let objRotate = [0, 0, 0];
   drawRotatedObj(obj, objRotate);
-  //noLoop()
+  // noLoop()
 }
 
 function update_angles(angles, changes){
@@ -205,18 +212,18 @@ function drawPainterly(camCoords, perspectiveFunc, fillColor=undefined) {
                                         );
   let canvasCoords = projectedCoords.map((coord)=>cartToCanvasCoords(coord.slice(0, 3)).slice(0, 2))
 
-  /*
   drawFace = (indices) => {
     for (let i = 0; i < indices.length; i++) {
       line(...canvasCoords[indices[i] - 1].map((c)=>c[0]), ...canvasCoords[indices[(i+1)%indices.length] - 1].map((c)=>c[0]));
     }
   }
-  */
+  /*
   drawFace = (indices) => {
       beginShape();
       indices.forEach((ind)=>vertex(...canvasCoords[ind - 1].map((c)=>c[0])));
       endShape(CLOSE);
     }
+  */
   
   if (fillColor)
     fill(fillColor);
@@ -231,13 +238,85 @@ function drawPainterly(camCoords, perspectiveFunc, fillColor=undefined) {
     strokeWeight(0);
     polygon.forEach((ind)=>point(...canvasCoords[ind - 1].map((c)=>c[0])));
   }
-
 }
 
-function drawRaster(camCoords, perspectiveFunc, fillColor=undefined) {
-  //hahah
-  const depthBuffer = Array(height).fill().map(()=>Array(width).fill(Infinity));
 
+function sameSide(p1,p2,a,b){
+  const p1_vec = createVector(...p1)
+  const p2_vec = createVector(...p2)
+  const a_vec = createVector(...a);
+  const b_vec = createVector(...b);
+  const c_prod1 = (p5.Vector.sub(b_vec, a_vec)).cross(p5.Vector.sub(p1_vec, a_vec));
+  const c_prod2 = (p5.Vector.sub(b_vec, a_vec)).cross(p5.Vector.sub(p2_vec, a_vec));
+  if (c_prod1.dot(c_prod2) >= 0)
+    return true;
+  else 
+    return false;
+}
+
+function flatten_vector(vec){
+  return vec.map(coord=>coord[0]);
+}
+
+function pointInTriangle(triangle, point){
+  //assumes point is 2d
+  const [a, b, c] = triangle.map(vert=>flatten_vector(vert).slice(0,2));
+  const p = flatten_vector(point)
+  if (sameSide(p,a,b,c) && sameSide(p,b,a,c)
+      && sameSide(p,c,a,b))
+    return true;
+  else 
+    return false;
+}
+
+function getDepth(triangle, point){
+  //assumes point is 2d and in triangle projection
+  const [a, b, c] = triangle.map(vec=>createVector(...flatten_vector(vec.slice(0, 3))))
+  const p = createVector(...flatten_vector(point));
+  const ab = p5.Vector.sub(b, a);
+  const ac = p5.Vector.sub(c, a);
+  const norm = p5.Vector.cross(ab, ac);
+  if (norm.z == 0)
+    throw new Error("triangle is degenerate");
+  //using equation of plane
+  const d = -(norm.x * a.x + norm.y * a.y + norm.z * a.z);
+  const depth = -(norm.x * p.x + norm.y * p.y + d) / norm.z;
+  return depth;
+}
+
+function scanlineRender(triangle, perspectiveFunc){
+  let projectedTri = triangle.map((coord)=>perspectiveFunc(coord, camera.viewVolume));
+  let canvasTri = projectedTri.map((coord)=>cartToCanvasCoords(coord.slice(0, 3)));
+  const minX = canvasTri.reduce((acc, vertex)=>Math.min(vertex[0], acc), Infinity);
+  const minY = canvasTri.reduce((acc, vertex)=>Math.min(vertex[1], acc), Infinity);
+  const maxX = canvasTri.reduce((acc, vertex)=>Math.max(vertex[0], acc), -Infinity);
+  const maxY = canvasTri.reduce((acc, vertex)=>Math.max(vertex[1], acc), -Infinity);
+
+  // console.log(canvasTri.map(vert=>vert.map(coord=>coord[0])), minX, minY, maxX, maxY);
+  // console.log(Math.max(0, Math.floor(minX)), Math.min(width - 1, Math.floor(maxX)), Math.max(0, Math.floor(minY)), Math.min(height - 1, Math.floor(maxY)))
+  for (let y=Math.max(0, Math.floor(minY)); y <= Math.min(height - 1, Math.floor(maxY)); y++){
+    for (let x=Math.max(0, Math.floor(minX)); x <= Math.min(width - 1, Math.floor(maxX)); x++){
+      //add depth
+      for (let i = 0; i < 3; i++)
+        canvasTri[i][2][0] = triangle[i][2][0]
+      const point = [[x + 0.5], [y + 0.5]];
+      if (pointInTriangle(canvasTri, point)){
+        const pointDepth = getDepth(canvasTri, point);
+        if (depthBuffer[y][x] >= pointDepth){
+          //fill pixel
+          square(x, y, 1);
+          depthBuffer[y][x] = pointDepth;
+        }
+      }
+    }
+  }
+}
+
+function drawRaster(camCoords, perspectiveFunc, fillColor="Black") {
+  stroke(fillColor)
+  for (const triangle of obj.faces){
+     scanlineRender(triangle.map(ind=>camCoords[ind - 1]), perspectiveFunc);
+  }
 }
 
 function drawRotatedObj(obj, angles) {
@@ -247,7 +326,8 @@ function drawRotatedObj(obj, angles) {
   obj.angles = update_angles(obj.angles, angles);
   ({ angles, base } = camera);
   let camCoords = worldCoords.map((coord)=>worldToCamera(coord, angles, base));
-  drawPainterly(camCoords, orthogonalProjectCoord, "orange");
+  drawRaster(camCoords, orthogonalProjectCoord, "orange");
+  // drawPainterly(camCoords, orthogonalProjectCoord, "orange");
 
   //color vertices
 }
