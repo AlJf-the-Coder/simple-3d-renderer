e92cf5fa6ef452f6771661465f96e4bc2ed11446
AlJf-the-Coder <agjafri@up.edu.ph>
2025-05-10 13:16:34 +0800

    refactor code

--- p5js_code/sketch.js
+++ p5js_code/sketch.js
@@ -1,16 +1,18 @@
 function preload() {
-    objects = loadJSON('./cube.json', 
+    // objects = loadJSON('./cube.json', 
+    objects = loadJSON('../objects.json', 
     (data) => console.log("successfully loaded objects"), 
     (err)=>console.log("error loading objects", err));
 }
 
 function setup() {
-  const width = 400;
-  const height = 400;
+  const width = 1000;
   frame_num = 0;
-  createCanvas(width, height);
-  camera = {base: [[0],[0],[0]], angles: [-Math.PI/4,0,0], viewVolume: {N:0, F:width, L:-width/2, R:width/2, T:height/2, B:-height/2}}
+  createCanvas(width, width);
+  camera = {base: [[0],[0],[0]], angles: [0,0,0], viewVolume: {N:width, F: 2 * width, L:-width/2, R:width/2, T:height/2, B:-height/2}};
+  far = camera.viewVolume.F;
   obj = objects.obj1;
+  obj.base = [0, 0, 2]
   depthBuffer = Array(height).fill().map(()=>Array(width).fill(Infinity));
   frameBuffer = Array(height).fill().map(()=>Array(width).fill([255, 255, 255]));
 
@@ -26,7 +28,7 @@ function draw() {
   text("frame_num: " + frame_num, 0, 0);
   frame_num++;
 
-  depthBuffer = Array(height).fill().map(()=>Array(width).fill(Infinity));
+  depthBuffer = Array(height).fill().map(()=>Array(width).fill(1));
   frameBuffer = Array(height).fill().map(()=>Array(width).fill([255, 255, 255]));
 
   loadPixels();
@@ -37,23 +39,11 @@ function draw() {
     }
   }
 
-  updatePixels();
-  
   // let camRotate = [0.3, 0, 0.5];
   // moveCamera(camera, undefined, camRotate);
   let objRotate = [-0.3, 0, -0.5];
   drawRotatedObj(obj, objRotate);
   
-  for (let y=0; y<height; y++){
-    for (let x=0; x<width; x++){
-      const index = 4 * (y * width + x);
-      pixels[index] = frameBuffer[y][x][0];
-      pixels[index + 1] = frameBuffer[y][x][1];
-      pixels[index + 2] = frameBuffer[y][x][2];
-    }
-  }
-
-  updatePixels();
   // noLoop()
 }
 
@@ -79,36 +69,52 @@ function matMultiply(matA, matB){
   return newMat;
 }
 
-function cartToCanvasCoords(homoCoord2d) {
-  if (homoCoord2d.length != 3)
-    throw new Error("coordinate is not a homogeneous 2d coordinate");
+function canonicalToFullCoords(homoCoord3d, viewVolume) {
+  if (homoCoord3d.length != 4)
+    throw new Error("coordinate is not a homogeneous 3d coordinate");
+  const {N,F,L,R,T,B} = viewVolume;
+  const mat = [
+    [(R-L)/2, 0, 0, (L+R)/2],
+    [0, (T-B)/2, 0, (T+B)/2],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+  ];
+  return matMultiply(mat, homoCoord3d);
+}
+function cartToCanvasCoords(homoCoord3d) {
+  if (homoCoord3d.length != 4)
+    throw new Error("coordinate is not a homogeneous 3d coordinate");
   const mat1 = [
-	  [1, 0, 0],
-	  [0, -1, 0], 
-	  [0, 0, 1]
+	  [1, 0, 0, 0],
+	  [0, -1, 0, 0], 
+	  [0, 0, 1, 0], 
+	  [0, 0, 0, 1]
   ]
   const mat2 = [
-	  [1, 0, width/2],
-	  [0, 1, height/2], 
-	  [0, 0, 1]
+	  [1, 0, 0, width/2],
+	  [0, 1, 0, height/2], 
+	  [0, 0, 1, 0], 
+	  [0, 0, 0, 1]
   ]
-  return matMultiply(mat2, matMultiply(mat1, homoCoord2d));
+  return matMultiply(mat2, matMultiply(mat1, homoCoord3d));
 }
 
-function canvasToCartCoords(homoCoord2d) {
-  if (homoCoord2d.length != 3)
-    throw new Error("coordinate is not a homogeneous 2d coordinate");
+function canvasToCartCoords(homoCoord3d) {
+  if (homoCoord3d.length != 4)
+    throw new Error("coordinate is not a homogeneous 3d coordinate");
   const mat1 = [
-	  [1, 0, 0],
-	  [0, -1, 0], 
-	  [0, 0, 1]
+	  [1, 0, 0, 0],
+	  [0, -1, 0, 0], 
+	  [0, 0, 1, 0]
+	  [0, 0, 0, 1]
   ]
   const mat2 = [
-	  [1, 0, -width/2],
-	  [0, 1, height/2], 
-	  [0, 0, 1]
+	  [1, 0, 0, -width/2],
+	  [0, 1, 0, height/2], 
+	  [0, 0, 1, 0]
+	  [0, 0, 0, 1]
   ]
-  return matMultiply(mat2, matMultiply(mat1, homoCoord2d));
+  return matMultiply(mat2, matMultiply(mat1, homoCoord3d));
 }
 
 function transformCoord(homoCoord3d, argPairs) {
@@ -184,7 +190,7 @@ function moveCamera(camera, translate=[0,0,0], rotate=[0,0,0]){
 function modelToWorld(homoCoord3d, scale, angles, base){
   if (homoCoord3d.length != 4)
     throw new Error("coordinate is not a homogeneous 3d coordinate");
-  const worldCoord = transformCoord(homoCoord3d, [[scaleCoord,scale], [rotateCoord,angles], [translateCoord,base]]);
+  const worldCoord = transformCoord(homoCoord3d, [[rotateCoord,angles], [translateCoord,base], [scaleCoord,scale]]);
   return worldCoord;
 }
 
@@ -196,14 +202,14 @@ function worldToCamera(homoCoord3d, angles, base){
   return camCoord;
 }
 
-function orthogonalProjectCoord(homoCoord3d, viewVolume) {
+function orthographicProjectCoord(homoCoord3d, viewVolume) {
   if (homoCoord3d.length != 4)
     throw new Error("coordinate is not a homogeneous 3d coordinate");
   const {N,F,L,R,T,B} = viewVolume;
   const projectMatrix = [
-	  [1,0,0,0],
-	  [0,1,0,0],
-	  [0,0,0,1],
+	  [2/(R-L),0,0,0],
+	  [0,2/(T-B),0,0],
+	  [0,0,1/(F-N),-N/(F-N)],
 	  [0,0,0,1]
   ];
   return matMultiply(projectMatrix, homoCoord3d);
@@ -214,63 +220,26 @@ function perspectiveProjectCoord(homoCoord3d, viewVolume) {
     throw new Error("coordinate is not a homogeneous 3d coordinate");
   const {N,F,L,R,T,B} = viewVolume;
   const projectMatrix = [
-	  [1,0,0,0],
-	  [0,1,0,0],
-	  [0,0,1,0],
+	  [(2*N)/(R-L),0,0,0],
+	  [0,(2*N)/(T-B),0,0],
+	  [0,0,F/(F-N),-(F*N)/(F-N)],
 	  [0,0,1,0]
   ];
   let projectedCoord = matMultiply(projectMatrix, homoCoord3d);
-  projectedCoord = projectedCoord.map((i)=>[i[0]/projectedCoord[3][0]]);
-  return projectedCoord;
+  const scale = projectedCoord[3][0];
+  const scaleMatrix = [
+    [1/scale,0,0,0],
+    [0,1/scale,0,0],
+    [0,0,1/scale,0],
+    [0,0,0,1/scale],
+  ];
+  return matMultiply(scaleMatrix, projectedCoord);
 }
 
-function drawPainterly(camCoords, perspectiveFunc, fillColor=undefined) {
-  let projectedCoords = camCoords.map((coord)=>perspectiveFunc(coord, camera.viewVolume));
-  const sortedPolygons = obj.faces.sort((poly1, poly2) => {
-                                              return Math.max(...poly2.map(ind=>camCoords[ind - 1][2])) - 
-                                                     Math.max(...poly1.map(ind=>camCoords[ind - 1][2]));
-                                              //return (poly2.reduce((acc, ind)=>camCoords[ind - 1][2] + acc, 0) / poly2.length) - 
-                                              //       (poly1.reduce((acc, ind)=>camCoords[ind - 1][2] + acc, 0) / poly1.length);
-                                              }
-                                        );
-  let canvasCoords = projectedCoords.map((coord)=>cartToCanvasCoords(coord.slice(0, 3)).slice(0, 2))
-
-  drawFace = (indices) => {
-    for (let i = 0; i < indices.length; i++) {
-      line(...canvasCoords[indices[i] - 1].map((c)=>c[0]), ...canvasCoords[indices[(i+1)%indices.length] - 1].map((c)=>c[0]));
-    }
-  }
-  /*
-  drawFace = (indices) => {
-      beginShape();
-      indices.forEach((ind)=>vertex(...canvasCoords[ind - 1].map((c)=>c[0])));
-      endShape(CLOSE);
-    }
-  */
-  
-  if (fillColor)
-    fill(fillColor);
-  else
-    noFill();
-
-  for (const polygon of sortedPolygons) {
-    stroke("green");
-    strokeWeight(1);
-    drawFace(polygon)
-    stroke("red");
-    strokeWeight(0);
-    polygon.forEach((ind)=>point(...canvasCoords[ind - 1].map((c)=>c[0])));
-  }
-}
 
 function flatten_vector(vec){
+  // return the 1d version of the matrix representation of vector 
   return vec.map(coord=>coord[0]);
-}
-
-function getDepth(triangle, barycentric){
-  const [m2, m3] = barycentric
-  const depth = triangle[0][2][0] * (1 - m2 - m3) + triangle[1][2][0] * m2 + triangle[2][2][0] * m3;
-  return depth;
 }
 
 function inverseMatrix2d(mat){
@@ -292,8 +261,15 @@ function isValidBarycentric(barycentric){
   return (m2 >= 0 && m3 >= 0 && m2 + m3 <= 1);
 }
 
+function getDepth(triangle, barycentric){
+  const [m2, m3] = barycentric
+  const depth = triangle[0][2][0] * (1 - m2 - m3) + triangle[1][2][0] * m2 + triangle[2][2][0] * m3;
+  return depth;
+}
+
 function getColor(barycentric){
-  colors = [[255,255,0], [255,0,0], [0,255,0]];
+  // colors = [[255,255,0], [255,0,0], [0,255,0]];
+  colors = [[255,255,0], [255,255,0], [255,255,0]];
   const [m2, m3] = barycentric;
   const m1 = 1 - m2 - m3;
   if ((m1 <1e-2) || (m2 <1e-2) || (m3 <1e-2))
@@ -306,12 +282,63 @@ function getColor(barycentric){
   }
 }
 
+function drawPainterly(camCoords, perspectiveFunc, fillColor=undefined) {
+  let projectedCoords = camCoords.map((coord)=>perspectiveFunc(coord, camera.viewVolume));
+  const sortedPolygons = obj.faces.sort((poly1, poly2) => {
+                                              return Math.max(...poly2.map(ind=>camCoords[ind - 1][2])) - 
+                                                     Math.max(...poly1.map(ind=>camCoords[ind - 1][2]));
+                                              }
+                                        );
+  let fullCoords = projectedCoords.map((coord)=>canonicalToFullCoords(coord, camera.viewVolume));
+  let canvasCoords = fullCoords.map((coord)=>cartToCanvasCoords(coord));
+
+  drawFace = (indices) => {
+      beginShape();
+      indices.forEach((ind)=>vertex(...flatten_vector(canvasCoords[ind - 1]).slice(0, 2)));
+      endShape(CLOSE);
+    }
+  
+  if (fillColor)
+    fill(fillColor);
+  else
+    noFill();
+
+  for (const polygon of sortedPolygons) {
+    stroke("green");
+    strokeWeight(1);
+    drawFace(polygon)
+    stroke("red");
+    strokeWeight(0);
+    polygon.forEach((ind)=>point(...flatten_vector(canvasCoords[ind - 1]).slice(0, 2)));
+  }
+}
+
+function drawWireframe(camCoords, perspectiveFunc, strokeColor="Black") {
+  let projectedCoords = camCoords.map((coord)=>perspectiveFunc(coord, camera.viewVolume));
+  let fullCoords = projectedCoords.map((coord)=>canonicalToFullCoords(coord, camera.viewVolume));
+  let canvasCoords = fullCoords.map((coord)=>cartToCanvasCoords(coord));
+
+  drawFace = (indices) => {
+    for (let i = 0; i < indices.length; i++) {
+      line(...flatten_vector(canvasCoords[indices[i] - 1]).slice(0, 2), ...flatten_vector(canvasCoords[indices[(i+1)%indices.length] - 1]).slice(0, 2));
+    }
+  }
+
+  obj.faces.forEach((polygon) => {
+      stroke(strokeColor);
+      strokeWeight(4);
+      drawFace(polygon)
+      stroke("Red");
+      strokeWeight(10);
+      polygon.forEach((ind)=>point(...flatten_vector(canvasCoords[ind - 1]).slice(0, 2)));
+    }
+  )
+}
+
 function scanlineRender(triangle, perspectiveFunc){
-  console.log(triangle)
   let projectedTri = triangle.map((coord)=>perspectiveFunc(coord, camera.viewVolume));
-  console.log(projectedTri)
-  throw new Error("stop");
-  let canvasTri = projectedTri.map((coord)=>cartToCanvasCoords(coord.slice(0, 3)));
+  let fullTri = projectedTri.map((coord)=>canonicalToFullCoords(coord, camera.viewVolume));
+  let canvasTri = fullTri.map((coord)=>cartToCanvasCoords(coord));
   const minX = canvasTri.reduce((acc, vertex)=>Math.min(vertex[0], acc), Infinity);
   const minY = canvasTri.reduce((acc, vertex)=>Math.min(vertex[1], acc), Infinity);
   const maxX = canvasTri.reduce((acc, vertex)=>Math.max(vertex[0], acc), -Infinity);
@@ -319,16 +346,12 @@ function scanlineRender(triangle, perspectiveFunc){
 
   for (let y=Math.max(0, Math.floor(minY)); y <= Math.min(height - 1, Math.floor(maxY)); y++){
     for (let x=Math.max(0, Math.floor(minX)); x <= Math.min(width - 1, Math.floor(maxX)); x++){
-      //add depth
-      for (let i = 0; i < 3; i++)
-        canvasTri[i][2][0] = triangle[i][2][0]
       const point = [[x + 0.5], [y + 0.5]];
       const barycentric = getBarycentric(canvasTri, point);
       if (isValidBarycentric(barycentric)){
-        // const pointDepth = getDepth(canvasTri, point);
         const pointDepth = getDepth(canvasTri, barycentric);
-        if (depthBuffer[y][x] >= pointDepth){
-          frameBuffer[y][x] = getColor(barycentric);
+        if (pointDepth >= 0 && pointDepth <= 1 && pointDepth <= depthBuffer[y][x]){
+          frameBuffer[y][x] = getColor(barycentric).map(color=>Math.floor((1-pointDepth)*color));
           depthBuffer[y][x] = pointDepth;
         }
       }
@@ -336,8 +359,7 @@ function scanlineRender(triangle, perspectiveFunc){
   }
 }
 
-function drawRaster(camCoords, perspectiveFunc, fillColor="Black") {
-  stroke(fillColor)
+function drawRaster(camCoords, perspectiveFunc) {
   for (const triangle of obj.faces){
      scanlineRender(triangle.map(ind=>camCoords[ind - 1]), perspectiveFunc);
   }
@@ -350,8 +372,23 @@ function drawRotatedObj(obj, angles) {
   let worldCoords = obj.vertices.map((coord)=>modelToWorld(coord, scale, obj.angles, base));
   ({ angles, base } = camera);
   let camCoords = worldCoords.map((coord)=>worldToCamera(coord, angles, base));
-  // drawRaster(camCoords, perspectiveProjectCoord, "orange");
-  drawRaster(camCoords, perspectiveProjectCoord, "orange");
+
+  const perspectiveFunc = orthographicProjectCoord;
+  // const perspectiveFunc = perspectiveProjectCoord;
+
+  drawRaster(camCoords, perspectiveFunc);
+
+  for (let y=0; y<height; y++){
+    for (let x=0; x<width; x++){
+      const index = 4 * (y * width + x);
+      pixels[index] = frameBuffer[y][x][0];
+      pixels[index + 1] = frameBuffer[y][x][1];
+      pixels[index + 2] = frameBuffer[y][x][2];
+    }
+  }
+
+  updatePixels();
+  drawWireframe(camCoords, perspectiveFunc, "Green");
 }
 
 /*
